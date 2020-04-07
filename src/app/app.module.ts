71c89095fd29d9e5b48d9dfc570c6fd20d065141
Adithya Sreyaj <adi.sreyaj@gmail.com>
2020-04-07 23:07:52 +0530

    ✨ Add mobile subheader controls

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,17 @@
  * File Created: Sunday, 5th April 2020 1:46:10 pm
  * Author: Adithya Sreyaj
  * -----
- * Last Modified: Tuesday, 7th April 2020 8:38:44 pm
+ * Last Modified: Tuesday, 7th April 2020 10:40:43 pm
  * Modified By: Adithya Sreyaj<adi.sreyaj@gmail.com>
  * -----
  */
 
-
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [AppComponent],
--- src/app/common/components/component.module.ts
+++ src/app/common/components/component.module.ts
@@ -4,22 +4,23 @@
  * File Created: Tuesday, 7th April 2020 8:16:25 pm
  * Author: Adithya Sreyaj
  * -----
- * Last Modified: Tuesday, 7th April 2020 8:38:58 pm
+ * Last Modified: Tuesday, 7th April 2020 10:50:44 pm
  * Modified By: Adithya Sreyaj<adi.sreyaj@gmail.com>
  * -----
  */
 
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { HeaderComponent } from './header/header.component';
 import { SubHeaderComponent } from './sub-header/sub-header.component';
 import { FooterComponent } from './footer/footer.component';
-import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [HeaderComponent, SubHeaderComponent, FooterComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, FlexLayoutModule],
   exports: [HeaderComponent, SubHeaderComponent, FooterComponent],
 })
 export class ComponentModule {}
