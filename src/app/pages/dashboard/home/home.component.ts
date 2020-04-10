d0cc3649ed123d733fd2355e0f244578bb6ab43d
Adithya Sreyaj <adi.sreyaj@gmail.com>
2020-04-10 20:23:16 +0530

    ✨ Add function to listen to notifications

--- src/app/pages/dashboard/home/home.component.ts
+++ src/app/pages/dashboard/home/home.component.ts
@@ -4,7 +4,7 @@
  * File Created: Tuesday, 7th April 2020 8:18:27 pm
  * Author: Adithya Sreyaj
  * -----
- * Last Modified: Friday, 10th April 2020 7:47:46 pm
+ * Last Modified: Friday, 10th April 2020 8:22:58 pm
  * Modified By: Adithya Sreyaj<adi.sreyaj@gmail.com>
  * -----
  */
@@ -57,6 +57,7 @@ export class HomeComponent implements OnInit {
     this.getQuickStats();
     this.getIndiaStates();
     this.getBookmarkedStates();
+    this.listenNotifications();
     // this.getLatestNews();
   }
 
@@ -173,6 +174,12 @@ export class HomeComponent implements OnInit {
       },
       () => {},
     );
+  }
+
+  private listenNotifications() {
+    this.afMessaging.messages.subscribe((message) => {
+      console.log(message);
+    });
   }
 
   private handleNotificationSaveError(err: any) {
